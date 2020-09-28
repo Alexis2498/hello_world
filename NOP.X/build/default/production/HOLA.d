e3a14d5502d54bd2c37105307c5d855453e61473
Alexis2498 <71996739+Alexis2498@users.noreply.github.com>
2020-09-27 21:35:19 -0500

    Add files via upload

---
+++ NOP.X/build/default/production/HOLA.d
@@ -0,0 +1,1 @@
+build/default/production\HOLA.i: HOLA.s
---
+++ NOP.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=NOP.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/NOP.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=nop.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/nop.x.tar
